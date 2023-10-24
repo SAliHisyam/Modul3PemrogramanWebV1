6a7ac10ebdf8a2ba3f0e998ce355dd08e989fe3e
SAliHisyam <127366847+SAliHisyam@users.noreply.github.com>
2023-10-24 15:16:36 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import About from "./pages/about";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import Contact from "./pages/contact";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="/about" Component={About} />
+        <Route path="/contact" Component={Contact} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Contact = () => {
+  return <div>Rumah</div>;
+};
+
+export default Contact;
